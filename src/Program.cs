f6341374010d378eeb8969ff8bc9b490236ade98
Devin Duplantis <37842419+dduplantis@users.noreply.github.com>
2020-04-06 17:23:18 -0500

    Begin working through WebApiClient guide from https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 
 namespace GitWebApiTest
 {
+
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
     class Program
     {
-        private static async Task ProcessRepositories()
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRespositories()
         {
-
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+            }
         }
 
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await ProcessRespositories();
         }
     }
 }
+
